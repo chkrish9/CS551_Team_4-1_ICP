e96f8926f6cccef27f15c9c218148fc6b4bcd6d0
Murali <murali.24.4.91@gmail.com>
2018-10-15 00:42:38 -0500

    Final code push

--- ICP-8/code/app.js
+++ ICP-8/code/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const clarifai = require('clarifai');
 const config = require('./config/config');
+const request = require('request');
 
 const clasrifaiApp = new clarifai.App({
     apiKey: config.key
@@ -27,7 +28,11 @@ app.get("/getName", (req, res, next) => {
     console.log(req.query.url);
     clasrifaiApp.models.predict("e466caa0619f444ab97497640cefc4dc",req.query.url).then(
         function (response) {
-            res.send(response.rawData.outputs[0].data.regions[0].data.face.identity.concepts[0].name);
+            request("https://kgsearch.googleapis.com/v1/entities:search?query=" + response.rawData.outputs[0].data.regions[0].data.face.identity.concepts[0].name + "&key=AIzaSyA9CvHV75OZgADhnju2DkS73y_3QS1Gsxo&limit=1&indent=True",{json:true},(error,reaponse,body)=>{
+                console.log(body.itemListElement);
+                res.send(body.itemListElement);
+            });
+            
         },
         function (err) {
             // there was an error
--- ICP-8/code/js/face.js
+++ ICP-8/code/js/face.js
@@ -4,19 +4,20 @@ app.controller("faceCtrl", function ($scope, $http) {
     $scope.url = "";
     $scope.imgurl = "default.png";
     $scope.name = "";
+    $scope.desc="";
 
     //In this method will make a get request to node api to get all records. 
     $scope.getName = function () {
         $scope.name="";
+        $scope.desc="";
         $http({
             method: 'GET',
             url: 'http://localhost:3002/getName?url=' + $scope.url
         }).then(function successCallback(response) {
             console.log(response.data);
             $scope.imgurl = $scope.url;
-            response.data.split(" ").forEach(element => {
-                $scope.name = $scope.name + element.charAt(0).toUpperCase() + element.slice(1)+" ";
-            });
+           $scope.name=response.data[0].result.name;
+           $scope.desc=response.data[0].result.description;
         }, function errorCallback(response) {
             console.log(response);
         });
